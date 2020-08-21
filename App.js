836bbaf97f514a13a81529ed7fca42357716729a
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-08-21 19:24:48 +0530

    firebase connection

---
+++ App.js
@@ -0,0 +1,141 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import 'react-native-gesture-handler';
+//import {View, Text,Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import TextArea from './info';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import {Textarea, Form} from 'native-base';
+const image = {
+  uri:
+    'https://fsb.zobj.net/crop.php?r=ZQ7JiYdnKX5QUSQEKVWSRnm6sosA7oLM2CuBlUC9kYhaPY--TJ_cKdhtO_KW1NHlJTiBQCKWV7qDLfNgmlDbDGUl-5H98vr4kkj-7uEgDYtlQ7D_GPTOEEvO6UWlF2sPao7j4VZ4D1doFVpR',
+};
+
+const image5 = {
+  uri:
+    'https://fsb.zobj.net/crop.php?r=ZQ7JiYdnKX5QUSQEKVWSRnm6sosA7oLM2CuBlUC9kYhaPY--TJ_cKdhtO_KW1NHlJTiBQCKWV7qDLfNgmlDbDGUl-5H98vr4kkj-7uEgDYtlQ7D_GPTOEEvO6UWlF2sPao7j4VZ4D1doFVpR',
+};
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <SafeAreaView style={[styles.container]}>
+        <ImageBackground source={image} style={styles.image}>
+          <View>
+            <Form>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Information')}>
+                <Text
+                  style={styles.button1}
+                  rowSpan={2}
+                  placeholderTextColor="#FFFFFF">
+                  WELCOME
+                </Text>
+                <Text
+                  style={styles.button1}
+            
+                  placeholderTextColor="#FFFFFF">
+                  WELCOME
+                </Text>
+
+                <Text style={styles.button}>START</Text>
+              </TouchableOpacity>
+            </Form>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={styles.container}>
+        <SafeAreaView style={[styles.container]}>
+          <ImageBackground source={image5} style={styles.image5}>
+            <ScrollView style={styles.scrollView}>
+              <TextArea />
+            </ScrollView>
+          </ImageBackground>
+        </SafeAreaView>
+      </View>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={HomeScreen} />
+
+        <Stack.Screen name="Information" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+  },
+  image5: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    borderWidth: 5,
+    // borderColor: '#b2f9d6',
+  },
+  scrollView: {
+    margin: 5,
+    flex: 1,
+    flexDirection: 'column',
+  },
+  button: {
+    fontSize: 30,
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    margin: 130,
+    color: '#FFFFFF',
+    marginTop: 320,
+    //  backgroundColor: '#F4FBF5',
+    borderRadius: 15,
+    borderRightColor: '#08a91c',
+  },
+  button1: {
+    fontSize: 40,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    borderColor: '#FFFFFF',
+    borderWidth: 5,
+    borderRadius: 5,
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    },
+});
---
+++ info/index.js
@@ -0,0 +1,116 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {Text, View, StyleSheet,Button,Alert,TouchableOpacity,SafeAreaView} from 'react-native';
+import {Container, Header, Content,Spinner, Textarea, Form,Icon,Item,Label,Input,Picker} from 'native-base';
+//import { SafeAreaView } from 'react-navigation';
+
+
+ 
+
+class TextArea extends Component {
+  render() {
+    return (
+       
+      <Form >
+          
+      <Item inlineLabel placeholderTextColor="#FFFFFF" >
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >First Name</Label>
+              <Input style={styles.button2} />
+            </Item>
+            <Item inlineLabel placeholderTextColor="#FFFFFF">
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >Last Name</Label>
+              <Input style={styles.button3}/>
+            </Item>
+            <Item inlineLabel placeholderTextColor="#FFFFFF">
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >Gender</Label>
+              <Input style={styles.button4}/>
+            </Item>
+            <Item inlineLabel placeholderTextColor="#FFFFFF">
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >Room</Label>
+              <Input style={styles.button5}/>
+            </Item>
+            <Item inlineLabel placeholderTextColor="#FFFFFF">
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >Site</Label>
+              <Input style={styles.button6}/>
+            </Item>
+            <Item inlineLabel placeholderTextColor="#FFFFFF">
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >City</Label>
+              <Input style={styles.button7}/>
+            </Item>
+            <Item inlineLabel placeholderTextColor="#FFFFFF">
+              <Label style={styles.button1} rowSpan={2}  placeholderTextColor="#FFFFFF" bordered placeholder="Your Name" >E-mail</Label>
+              <Input style={styles.button8}/>
+            </Item>
+            
+        <SafeAreaView >
+        <TouchableOpacity  
+      
+    onPress={() => Alert.alert('Success Submition')}>
+         
+           <Text style={styles.button} >SUBMIT</Text>
+ 
+        </TouchableOpacity>
+      
+      </SafeAreaView>
+      </Form>
+     
+      
+    );
+  }
+}
+const styles = StyleSheet.create({
+  button: {
+    fontSize:30,
+  color:'#0AFFFF',
+    marginLeft:130,
+    //backgroundColor:'#00ffb2',
+    width:160,
+    borderRadius:10, 
+    fontWeight:'bold',
+    marginVertical:25,
+  },
+  button1: {
+    fontSize:20,
+  color:'#FFFFFF',
+    fontWeight:'bold',
+
+  },
+  button2:{
+color:'#FFFFFF',
+//marginLeft:5,
+  },
+  button3:{
+    color:'#FFFFFF',
+    marginLeft:5,
+      },
+      button4:{
+        color:'#FFFFFF',
+        marginLeft:3,
+          },
+          button5:{
+            color:'#FFFFFF',
+            marginLeft:52,
+              },
+              button6:{
+                color:'#FFFFFF',
+                marginLeft:68,
+                  },
+                  button7:{
+                    color:'#FFFFFF',
+                    marginLeft:68,
+                      },
+                      button8:{
+                        color:'#FFFFFF',
+                        marginLeft:48,
+                          },
+
+ icon: {
+ flex:3, 
+  marginLeft:250,
+marginTop:-58,
+color:'#FFFFFF',
+
+  },
+
+});
+export default TextArea;
